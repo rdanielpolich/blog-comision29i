eb6e4c02e06a9b7539d2e025b78093616f1cc751
unknown <rdanielpolich@gmail.com>
2022-07-09 14:03:26 -0300

    cambios de estilos

--- src/pages/PostScreen.jsx
+++ src/pages/PostScreen.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 class Post {
-  constructor(id, titulo, texto, userid, fecha) {
+  constructor(id, titulo, texto, userId, fecha) {
     this.id = id;
     this.titulo = titulo;
     this.texto = texto;
-    this.userid = userid;
+    this.userId = userId;
     this.fecha = fecha;
   }
 }
@@ -13,16 +13,16 @@ class Post {
 const PostScreen = () => {
   return (
     <div>
-      <div className="container col-12 col-md-8 offset-md-2 mt-3">
+      <div className="container col-12 col-md-8 offset-md-2 mt-3 justify-content-center ">
         <h1>POSTS</h1>
         <div className="row">
           <div className="col">
-            <h3>{Post.titulo} titulo</h3>
-            <div>{Post.texto} texto</div>
+            <h3>{Post.titulo}</h3>
+            <div>{Post.texto}</div>
           </div>
           <div className="row">
             <div className="col col-6">
-              <h5>posteado por {Post.userid}</h5>
+              <h5>Posteado por: {Post.userId}</h5>
             </div>
             <div className="col col-6">
               <h5>{Post.fecha} fecha</h5>
