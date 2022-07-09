a9c6d29afa3d1762228369b1d43e38ff4b35bfcc
unknown <rdanielpolich@gmail.com>
2022-07-09 13:55:00 -0300

    creacion PostScreen

--- src/pages/PostScreen.jsx
+++ src/pages/PostScreen.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+
+class Post {
+  constructor(id, titulo, texto, userid, fecha) {
+    this.id = id;
+    this.titulo = titulo;
+    this.texto = texto;
+    this.userid = userid;
+    this.fecha = fecha;
+  }
+}
 
 const PostScreen = () => {
   return (
     <div>
-      
+      <div className="container col-12 col-md-8 offset-md-2 mt-3">
+        <h1>POSTS</h1>
+        <div className="row">
+          <div className="col">
+            <h3>{Post.titulo} titulo</h3>
+            <div>{Post.texto} texto</div>
+          </div>
+          <div className="row">
+            <div className="col col-6">
+              <h5>posteado por {Post.userid}</h5>
+            </div>
+            <div className="col col-6">
+              <h5>{Post.fecha} fecha</h5>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostScreen
+export default PostScreen;
